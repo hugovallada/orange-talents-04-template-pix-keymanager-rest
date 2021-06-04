3f70ef836e93544bdb79e71266a0523ea360db18
hugovallada <valladahugo@gmail.com>
2021-06-04 17:32:27 -0300

    Cria o grpc client factory e configura o channel

---
+++ src/main/kotlin/br/com/zup/hugovallada/util/GrpcClientFactory.kt
@@ -0,0 +1,16 @@
+package br.com.zup.hugovallada.util
+
+import br.com.zup.hugovallada.KeyManagerGrpcServiceGrpc
+import io.grpc.ManagedChannel
+import io.micronaut.context.annotation.Factory
+import io.micronaut.grpc.annotation.GrpcChannel
+import javax.inject.Singleton
+
+@Factory
+class GrpcClientFactory {
+
+    @Singleton
+    fun cadastrarClientStub(@GrpcChannel(value = "pix-manager") channel: ManagedChannel): KeyManagerGrpcServiceGrpc.KeyManagerGrpcServiceBlockingStub?{
+        return KeyManagerGrpcServiceGrpc.newBlockingStub(channel)
+    }
+}
